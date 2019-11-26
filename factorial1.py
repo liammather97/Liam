121ee6f2307bb062c264694196fec1e4081ff323
liammather97 <liammather97@gmail.com>
2019-11-26 15:34:20 +0000

    commit

---
+++ factorial1.py
@@ -0,0 +1,22 @@
+def factorial(number):
+    count = number
+    if number == 0:
+        return number
+    else:
+        for i in range(1,count):
+            print ("i is: " ,i)
+            number *= i
+            print (number)
+        return number
+def reverse_factorial(number):
+    count = number
+    if number ==0:
+        return number
+    else:
+        while number > 1:
+            for i in range(1,(count+1)):
+                print ("i is: ", i)
+                number /=i
+                if number ==1:
+                    return i
+        return "None"
