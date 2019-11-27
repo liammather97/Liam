8d81cf91f2e1f8e59eed819322ce59cd7b38614b
liammather97 <liammather97@gmail.com>
2019-11-27 10:14:29 +0000

    jenkins commit

--- tests/factorial_test.py
+++ tests/factorial_test.py
@@ -5,5 +5,14 @@ from application import factorial
 def test_correct():
     assert factorial.factorial(5)==120
 
+def test_big():
+    assert factorial.factorial(13)==6227020800
+
+def test_bigger():
+    assert factorial.factorial(20)==2432902008176640000
+
+def test_evenbigger():
+    assert factorial.factorial(25)==15511210043330985984000000
+
 
     
